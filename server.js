1f0c65631972b381639b6a2ad0a2fff2ca289f95
ikoral <ilhankoral@outlook.com>
2020-12-24 12:06:27 +0100

    dependencies modified v2

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/users", users);
 //Handle Production
 if (process.env.NODE_ENV === "production") {
   // Static Folder
-  app.use(express.static(__dirname + "/public/"));
+  app.use(express.static(path.join(__dirname, "public")));
 
   //Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
