f6415d5839448f754e897a5f5909789f079f6023
ikoral <ilhankoral@outlook.com>
2020-12-24 12:46:22 +0100

    production settings changed v3 for heroku

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Identify Routes
+process.env.RESET_URL = "/reset";
 const users = require("./routes/users");
 app.use("/api/users", users);
 
 //Handle Production
 if (process.env.NODE_ENV === "production") {
   // Static Folder
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(__dirname + "/public/"));
 
   //Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
